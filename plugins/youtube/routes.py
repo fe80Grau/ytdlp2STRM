ea1f9df1a66bc4890bd609c7cce68b238ac6e98c
David Grau <fe80grau@gmail.com>
2023-06-01 19:11:45 +0200

    project folder structure

--- plugins/youtube/routes.py
+++ plugins/youtube/routes.py
@@ -1,5 +1,5 @@
 from __main__ import app
-from plugins.youtube.main import direct, bridge, download
+from plugins.youtube.youtube import direct, bridge, download
 from flask import Flask, stream_with_context, request, Response, send_from_directory, send_file, redirect
 import json
 
@@ -17,9 +17,19 @@ with open(
 def youtube_direct(youtube_id):
     return direct(youtube_id)
 
+#Keep URL from v0 version
+@app.route("/youtube/redirect/<youtube_id>")
+def youtube_redirect(youtube_id):
+    return direct(youtube_id)
+
 #Stream data directly throught http (no serve video duration info, no disk usage)
 @app.route("/youtube/bridge/<youtube_id>")
 def youtube_bridge(youtube_id):
+    return bridge(youtube_id)
+
+#Keep URL from v0 version
+@app.route("/youtube/stream/<youtube_id>")
+def youtube_stream(youtube_id):
     return bridge(youtube_id)
 
 #Download video and semd data throught http (serve video duration info, disk usage **clean_old_videos fucntion save your money)
