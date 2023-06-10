0be1d76156ab5374b2d6178f438bd6b4f5537e52
David Grau <fe80grau@gmail.com>
2023-06-10 19:04:00 +0200

    crunchyroll plugin

--- plugins/crunchyroll/crunchyroll.py
+++ plugins/crunchyroll/crunchyroll.py
@@ -26,8 +26,8 @@ with open(
 media_folder = config["strm_output_folder"]
 channels_list = config["channels_list_file"]
 cookies_file = config["crunchyroll_cookies_file"]
-lang_audio_subtitle = config["crunchyroll_lang_audio_subtitle"]
-
+subtitle_language = config["crunchyroll_subtitle_language"]
+audio_language = config['crunchyroll_audio_language']
 
 ##Utils | Read and download full channels, generate nfo and strm files
 def channels():
@@ -62,48 +62,57 @@ def to_strm(method):
         print("Processing videos in channel")
 
         command = ['yt-dlp', 
-                    '--cookies', '"{}"'.format(cookies_file),
-                    '--print', '"%(season_number)s;%(season)s;%(episode_number)s;%(episode)s;%(webpage_url)s;%(quality)s"', 
+                    '--cookies', '{}'.format(cookies_file),
+                    '--print', '%(season_number)s;%(season)s;%(episode_number)s;%(episode)s;%(webpage_url)s', 
                     '--no-download',
                     '--no-warnings',
-                    '--extractor-args', '"crunchyrollbeta:hardsub={}"'.format(lang_audio_subtitle),
+                    '--match-filter', 'language={}'.format(audio_language),
+                    '--extractor-args', 'crunchyrollbeta:hardsub={}'.format(subtitle_language),
                     '{}'.format(crunchyroll_channel_url)]
     
 
 
         #print("Command \n {}".format(' '.join(command)))
-        lines = subprocess.getoutput(' '.join(command)).split('\n')
+        #lines = subprocess.getoutput(' '.join(command)).split('\n')
 
-        for line in lines:
-            if line != "" and not 'ERROR' in line and not 'WARNING' in line:
-                print(line)
-                quality = str(line).rstrip().split(';')[5]
-                season_number = str(line).rstrip().split(';')[0]
-                season = str(line).rstrip().split(';')[1]
-                episode_number = (line).rstrip().split(';')[2]
-                episode = (line).rstrip().split(';')[3]
-                url = (line).rstrip().split(';')[4].replace('https://www.crunchyroll.com/','').replace('/','_')
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, universal_newlines=True)
+        try:
+            for line in iter(process.stdout.readline, b''):
+                #print(line)
+                if line != "" and not 'ERROR' in line and not 'WARNING' in line:
+                    
+                    season_number = str(line).rstrip().split(';')[0].zfill(2)
+                    season = str(line).rstrip().split(';')[1]
+                    episode_number = (line).rstrip().split(';')[2].zfill(4)
+                    episode = (line).rstrip().split(';')[3]
+                    url = (line).rstrip().split(';')[4].replace('https://www.crunchyroll.com/','').replace('/','_')
 
 
-                video_name = "{} [{}]".format("S{}E{} - {}".format(season_number, episode_number, episode), quality)
+                    video_name = "{} - {}".format("S{}E{}".format(season_number, episode_number), episode)
 
 
-                file_content = "http://{}:{}/{}/{}/{}".format(host, port, source_platform, method, url)
-                file_path = "{}/{}/{}/{}.{}".format(media_folder,  sanitize("{}".format(crunchyroll_channel_folder)),  sanitize(season), sanitize(video_name), "strm")
-                make_clean_folder(file_path)
-                data = {
-                    "video_id" : quality, 
-                    "video_name" : video_name
-                }
-                if not os.path.isfile(file_path):
-                    write_file(file_path, file_content)
+                    file_content = "http://{}:{}/{}/{}/{}".format(host, port, source_platform, method, url)
+                    file_path = "{}/{}/{}/{}.{}".format(media_folder,  sanitize("{}".format(crunchyroll_channel_folder)),  sanitize("S{} - {}".format(season_number, season)), sanitize(video_name), "strm")
+                    make_clean_folder("{}/{}/{}".format(media_folder,  sanitize("{}".format(crunchyroll_channel_folder)),  sanitize("S{} - {}".format(season_number, season))))
+                    data = {
+                        "video_name" : video_name
+                    }
+                    if not os.path.isfile(file_path):
+                        write_file(file_path, file_content)
 
-                print(data)
+                    print(data)
+
+                if not line: break
+                
+        finally:
+            process.kill()
+
 
     return True 
 
 
 ##Video data stream | direct, bridge and download mode
 def direct(crunchyroll_id): #Sponsorblock doesn't work in this mode
-    crunchyroll_url = subprocess.getoutput("yt-dlp -f best --no-warnings {} --get-url".format(crunchyroll_id.replace('_','/')))
+    crunchyroll_url = subprocess.getoutput("yt-dlp -f best --cookies {} --no-warnings --match-filter language={} --extractor-args crunchyrollbeta:hardsub={} https://www.crunchyroll.com/{} --get-url".format(cookies_file, audio_language, subtitle_language, crunchyroll_id.replace('_','/')))
+    #print(crunchyroll_url)
     return redirect(crunchyroll_url, code=301)
