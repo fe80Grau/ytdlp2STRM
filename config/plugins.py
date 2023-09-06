3c1b6185b8051040d338e555afa844282a8653c8
fe80grau@gmail.com <fe80grau@gmail.com>
2023-09-06 17:12:26 +0200

    gui, crons, readme

--- config/plugins.py
+++ config/plugins.py
@@ -1,6 +1,6 @@
 import experiments.experiments
 from plugins.youtube import youtube
-#from plugins.twitch import twitch
+from plugins.twitch import twitch
 from plugins.crunchyroll import crunchyroll
 from plugins.sx3 import sx3
 from plugins.pokemon_tv import pokemon_tv
