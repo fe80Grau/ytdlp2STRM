7f2b98923f9fe3a5a7ed0725c483f74e79eb6d9f
fe80grau@gmail.com <fe80grau@gmail.com>
2023-09-06 18:20:53 +0200

    css js clean and responsive

---
+++ ui/static/bs-theme-mode.js
@@ -0,0 +1,15 @@
+(function () {
+    const htmlElement = document.querySelector("html")
+    if(htmlElement.getAttribute("data-bs-theme") === 'auto') {
+        function updateTheme() {
+          document.querySelector("html").setAttribute(
+            "data-bs-theme",
+            window.matchMedia(
+              "(prefers-color-scheme: dark)"
+            ).matches ? "dark" : "light"
+          )
+        }
+        window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', updateTheme)
+        updateTheme()
+    }
+    })()
