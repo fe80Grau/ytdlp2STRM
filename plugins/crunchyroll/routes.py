e0673cd9c0b39a1f621780f675d9805dac87770f
fe80grau@gmail.com <fe80grau@gmail.com>
2024-04-09 20:29:51 +0200

    crunchyroll fix v1, needs yt-dlp nightly - timezone field cron

--- plugins/crunchyroll/routes.py
+++ plugins/crunchyroll/routes.py
@@ -1,7 +1,11 @@
 from __main__ import app, request
-from plugins.crunchyroll.crunchyroll import direct
+from plugins.crunchyroll.crunchyroll import direct, download
 
 ### CRUNCHY ZONE
 @app.route("/crunchyroll/direct/<crunchyroll_id>")
 def crunchyroll_direct(crunchyroll_id):
     return direct(crunchyroll_id)
+#Download video and semd data throught http (serve video duration info, disk usage **clean_old_videos fucntion save your money)
+@app.route("/crunchyroll/download/<crunchyroll_id>")
+def crunchyroll_download(crunchyroll_id):
+    return download(crunchyroll_id)
