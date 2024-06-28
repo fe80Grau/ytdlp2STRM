13057657c1fa82112153d5be1c2a4b3035b4dbb8
David Grau <fe80grau@gmail.com>
2024-06-28 20:06:10 +0200

    Disable crunchy

--- config/plugins.py
+++ config/plugins.py
@@ -1,7 +1,7 @@
 import experiments.experiments
 from plugins.youtube import youtube
 from plugins.twitch import twitch
-from plugins.crunchyroll import crunchyroll
+#from plugins.crunchyroll import crunchyroll
 from plugins.pokemon_tv import pokemon_tv
 from plugins.tv3cat import tv3cat
 #from plugins.telegram import telegram
